727e8d439ffb204df745149acd78c7a75d1f7ac8
Enderpalm <ch.enderpalm@gmail.com>
2023-03-01 23:14:12 +0700

    code: get InputStream with fallback hostname

---
+++ src/main/groovy/dev/enderpalm/pipette/util/VersionRetriever.groovy
@@ -0,0 +1,19 @@
+package dev.enderpalm.pipette.util
+
+class VersionRetriever {
+
+    // Hostname for the Fabric's web service
+    String[] meta = ["https://meta.fabricmc.net", "https://meta2.fabricmc.net"]
+    String[] maven = ["https://meta.fabricmc.net", "https://meta2.fabricmc.net"]
+
+    static InputStream getInputStream(String[] hostname, String path) {
+        for (String host : hostname) {
+            try {
+                return new URL(host + path).openConnection().getInputStream()
+            } catch (Exception e) {
+                e.printf("Pipette: Failed to connect to %s :(", host + path)
+            }
+        }
+        throw new Exception("Pipette: Failed to connect to any host :(")
+    }
+}
