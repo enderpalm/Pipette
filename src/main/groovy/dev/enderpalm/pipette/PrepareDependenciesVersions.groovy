fcfc338b38e0b19fe9438ff42da3482c0ddb7966
Enderpalm <ch.enderpalm@gmail.com>
2023-03-01 22:59:53 +0700

    code: add a Minecraft version as arg

--- src/main/groovy/dev/enderpalm/pipette/PrepareDependenciesVersions.groovy
+++ src/main/groovy/dev/enderpalm/pipette/PrepareDependenciesVersions.groovy
@@ -1,7 +1,18 @@
 package dev.enderpalm.pipette
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.options.Option
 
 abstract class PrepareDependenciesVersions extends DefaultTask{
+
+    private String minecraft
+    private String loader
+    private String yarn
+    private String fabric
+
+    @Option(option = "minecraft", description = "Minecraft version")
+    void setMinecraftVersion(String minecraft) {
+        this.minecraft = minecraft
+    }
 
 }
