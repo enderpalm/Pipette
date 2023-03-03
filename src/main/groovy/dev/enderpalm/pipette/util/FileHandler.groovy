b2815ecc6317f1cd9d1af2f2fc4bc86353df84e2
Enderpalm <ch.enderpalm@gmail.com>
2023-03-03 14:50:01 +0700

    fix: wrong property name in Fabric mod json

--- src/main/groovy/dev/enderpalm/pipette/util/FileHandler.groovy
+++ src/main/groovy/dev/enderpalm/pipette/util/FileHandler.groovy
@@ -20,11 +20,12 @@ class FileHandler {
         }
     }
 
-    void modifyFabricModJson(File projectDir, String loader, String minecraft) {
+    void modifyFabricModJson(File projectDir, String loader, String minecraft, String java) {
         def path = Paths.get(projectDir.getAbsolutePath(), "src", "main", "resources", "fabric.mod.json")
         def json = new JsonSlurper().parseText(path.toFile().text)
         json.depends.fabricloader = ">=$loader"
         json.depends.minecraft = "~$minecraft"
+        json.depends.java = ">=$java"
         path.toFile().text = JsonOutput.prettyPrint(JsonOutput.toJson(json))
     }
 }
