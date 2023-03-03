e15e6b476c4db52426a4d1afcdee21ef22ce18c6
Enderpalm <ch.enderpalm@gmail.com>
2023-03-03 16:25:38 +0700

    code: add `modifyMixinJson()` and tested via external project

--- src/main/groovy/dev/enderpalm/pipette/PrepareDependencyVersionsTask.java
+++ src/main/groovy/dev/enderpalm/pipette/PrepareDependencyVersionsTask.java
@@ -40,11 +40,14 @@ public class PrepareDependencyVersionsTask extends DefaultTask {
         }
         var loader = retriever.getLatestLoaderVersion();
         var java = retriever.getJavaVersion(this.minecraft, stable);
+
         this.properties.put("minecraft_version", this.minecraft);
         this.properties.put("loader_version", loader);
         this.properties.put("yarn_mappings", retriever.getYarnMappingVersion(this.minecraft));
         this.properties.put("fabric_version", retriever.getFabricApiVersion(this.minecraft, stable));
+
         fileHandler.modifyGradleProperties(project.getAnt(), this.properties);
-        fileHandler.modifyFabricModJson(project.getProjectDir(), loader, this.minecraft, java.get(1));
+        Object json = fileHandler.modifyFabricModJson(project.getProjectDir(), loader, this.minecraft, java);
+        fileHandler.modifyMixinJson(project.getProjectDir(), json, java);
     }
 }
--- src/main/groovy/dev/enderpalm/pipette/util/FileHandler.groovy
+++ src/main/groovy/dev/enderpalm/pipette/util/FileHandler.groovy
@@ -20,12 +20,24 @@ class FileHandler {
         }
     }
 
-    void modifyFabricModJson(File projectDir, String loader, String minecraft, String java) {
+    Object modifyFabricModJson(File projectDir, String loader, String minecraft, int java) {
         def path = Paths.get(projectDir.getAbsolutePath(), "src", "main", "resources", "fabric.mod.json")
         def json = new JsonSlurper().parseText(path.toFile().text)
         json.depends.fabricloader = ">=$loader"
         json.depends.minecraft = "~$minecraft"
         json.depends.java = ">=$java"
         path.toFile().text = JsonOutput.prettyPrint(JsonOutput.toJson(json))
+        return json
+    }
+
+    void modifyMixinJson(File projectDir, Object fabricJson ,int java){
+        String[] mixinList = fabricJson.mixins
+        Iterator mixin = mixinList.iterator()
+        while (mixin.hasNext()) {
+            def path = Paths.get(projectDir.getAbsolutePath(), "src", "main", "resources", mixin.next())
+            def json = new JsonSlurper().parseText(path.toFile().text)
+            json.compatibilityLevel = "JAVA_$java"
+            path.toFile().text = JsonOutput.prettyPrint(JsonOutput.toJson(json))
+        }
     }
 }
